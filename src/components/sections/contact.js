bdc3b36a53df24e9f0a2cb13c2acd2d6fe97a475
dong-alex <adong@ualberta.ca>
2020-05-25 05:44:49 -0600

    refactor redirect links

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -1,15 +1,10 @@
 import React from "react"
 import styled from "styled-components"
 import Container from "react-bootstrap/Container"
-import LinkedInLogo from "../static/linkedin.svg"
-import GithubLogo from "../static/github.svg"
+import LinkedInLogo from "../../static/linkedin.svg"
+import GithubLogo from "../../static/github.svg"
 
 const ContactContainer = styled(Container)`
-  display: flex;
-  justify-content: center;
-`
-
-const Header = styled.span`
   display: flex;
   justify-content: center;
 `
--- src/pages/thank-you.js
+++ src/pages/thank-you.js
@@ -1,18 +1,30 @@
 import React from "react"
 import Layout from "../components/layout"
+import ContactSection from "../components/sections/contact"
+import styled from "styled-components"
+
+const Header = styled.span`
+  display: flex;
+  margin-bottom: 1rem;
+  justify-content: center;
+`
+
+const ThankYouMessage = styled.p`
+  text-align: center;
+`
 
 const ThankYouPage = () => {
-	return (
-		<Layout>
-			<h1>
-				Contact
-			</h1>
-			<p>
-				Thank you for your submission! Feel free to connect below as well.
-			</p>
-			
-		</Layout>
-	)
+  return (
+    <Layout>
+      <Header>
+        <h1>Contact</h1>
+      </Header>
+      <ThankYouMessage>
+        Thank you for your submission! Feel free to connect below as well.
+      </ThankYouMessage>
+      <ContactSection />
+    </Layout>
+  )
 }
 
 export default ThankYouPage
