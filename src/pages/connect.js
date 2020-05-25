2f71e0c45c85d88ebb9093d4749d8ff58c7a98c3
dong-alex <adong@ualberta.ca>
2020-05-25 05:35:43 -0600

    customize thank you section

--- src/pages/connect.js
+++ src/pages/connect.js
@@ -7,23 +7,12 @@ import Container from "react-bootstrap/Container"
 import SEO from "../components/seo"
 import LinkedInLogo from "../static/linkedin.svg"
 import GithubLogo from "../static/github.svg"
-import NetlifyForm from "../components/netlifyForm"
-
-const ContactContainer = styled(Container)`
-  display: flex;
-  justify-content: center;
-`
+import NetlifyForm from "../components/sections/netlifyForm"
+import ContactSection from "../components/sections/contact"
 
 const Header = styled.span`
   display: flex;
   justify-content: center;
-`
-
-const ContactLogo = styled.img`
-  margin: 2rem;
-  &:hover {
-    transform: scale(1.1);
-  }
 `
 
 const ConnectPage = () => {
@@ -35,18 +24,7 @@ const ConnectPage = () => {
       <Header>
         <h2>Connect</h2>
       </Header>
-      <ContactContainer>
-        <a href="https://github.com/dong-alex" target="_blank" rel="noreferrer">
-          <ContactLogo src={GithubLogo} alt="github" />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/dongalex/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <ContactLogo src={LinkedInLogo} alt="linkedin" />
-        </a>
-      </ContactContainer>
+      <ContactSection />
       <NetlifyForm />
     </Layout>
   )
---
+++ src/pages/thank-you.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const ThankYouPage = () => {
+	return (
+		<Layout>
+			<h1>
+				Contact
+			</h1>
+			<p>
+				Thank you for your submission! Feel free to connect below as well.
+			</p>
+			
+		</Layout>
+	)
+}
+
+export default ThankYouPage
