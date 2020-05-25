6ad61edfcb11f7a0548915219dd723317d670afb
dong-alex <adong@ualberta.ca>
2020-05-25 16:06:13 -0600

    add google captcha capability

--- src/components/sections/netlifyForm.js
+++ src/components/sections/netlifyForm.js
@@ -1,5 +1,6 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import styled from "styled-components"
+import ReCAPTCHA from "react-google-recaptcha"
 
 const Header = styled.span`
   display: flex;
@@ -7,7 +8,42 @@ const Header = styled.span`
   justify-content: center;
 `
 
+const CaptchaContainer = styled(ReCAPTCHA)`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 1rem;
+`
+
 const NetlifyForm = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [message, setMessage] = useState("")
+  const [enableCaptcha, setEnableCaptcha] = useState(false)
+
+  const isFormComplete = () => {
+    return name !== "" && email !== "" && message !== ""
+  }
+
+  useEffect(() => {
+    if (isFormComplete()) {
+      setEnableCaptcha(true)
+    } else {
+      setEnableCaptcha(false)
+    }
+  }, [message, email, name])
+
+  const handleMessageChange = event => {
+    setMessage(event.target.value)
+  }
+
+  const handleEmailChange = event => {
+    setEmail(event.target.value)
+  }
+
+  const handleNameChange = event => {
+    setName(event.target.value)
+  }
+
   return (
     <>
       <Header>
@@ -17,6 +53,7 @@ const NetlifyForm = () => {
         name="contact"
         method="POST"
         data-netlify="true"
+        data-netlify-recaptcha="true"
         action="/thank-you"
       >
         <input type="hidden" name="form-name" value="contact" />
@@ -30,6 +67,8 @@ const NetlifyForm = () => {
             class="form-control"
             aria-label="e-mail"
             aria-describedby="email-field"
+            value={email}
+            onChange={handleEmailChange}
           />
         </div>
         <div class="input-group mb-3">
@@ -42,6 +81,8 @@ const NetlifyForm = () => {
             name="name"
             aria-label="Name"
             aria-describedby="name"
+            value={name}
+            onChange={handleNameChange}
           />
         </div>
         <div class="input-group mb-3">
@@ -52,8 +93,13 @@ const NetlifyForm = () => {
             class="form-control"
             aria-label="With textarea"
             name="message"
+            value={message}
+            onChange={handleMessageChange}
           />
         </div>
+        {enableCaptcha && (
+          <CaptchaContainer sitekey={process.env.GATSBY_RECAPTCHA_KEY} />
+        )}
         <input type="submit" value="Submit" class="btn btn-dark btn-block" />
       </form>
     </>
